c2c5bf449b07d1090b2b36771059b6b847f20dde
Denzale Houston <denzalethouston@gmail.com>
2020-10-13 17:28:46 -0700

    inclued more jquery questions

--- Node.js/README.md
+++ Node.js/README.md
@@ -1,2 +1,28 @@
-# ds
-## sd
+# gaga
+    # Table of Contents
+    
+    -[Description](#description)
+    -[Installation](#installation)
+    -[Usage](#usage)
+    -[License](#license)
+    -[Contributing](#contributing)
+    -[Tests](#tests)
+    -[Questions](#questions)
+
+## Description:
+    its cool asf
+## Installation:
+    none
+## Usage:
+    idk
+## License:
+
+## Contributing
+    my firend
+## Test
+    baba
+## Question
+    If you have any questions you can go to my Github page through this link:
+    -[Github](http://github.com/denzalethouston)
+
+    For additional questions please reach out to my email at: superdenzie@gmail.com.
--- Node.js/script.js
+++ Node.js/script.js
@@ -4,27 +4,52 @@ const util = require("util");
 
 inquirer
     .prompt([
+        //Project Name
         {
             type: "input",
-            name: "name",
-            message: "what is your name?"
+            name: "title",
+            message: "what is the name of your Project?"
+        },
+        //
+        {
+            type: "input",
+            name: "description",
+            message: "A description of your Project"
         },
         {
             type: "input",
-            name: "location",
-            message: "where are you from?"
+            name: "installation",
+            message: "What are the installation for this project? Write NONE if none"
         },
         {
             type: "input",
-            name: "location",
-            message: "where are you from?"
+            name: "usage",
+            message: "How would you like your application to be used?"
         },
         {
             type: "input",
-            name: "location",
-            message: "where are you from?"
+            name: "contributing",
+            message: "Who is contibuting in this project?"
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "what are the test instructions?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your Github username?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "what is your email address?"
         },
     ])
+
+
+
     .then(async data => {
         const readme = await generateREADME(data);
         console.log(readme)
@@ -38,8 +63,37 @@ inquirer
     })
 
 
+
+
 function generateREADME(data) {
-    const md = `# ${data.name}
-## ${data.location}`
+    const md = `# ${data.title}
+    # Table of Contents
+    
+    -[Description](#description)
+    -[Installation](#installation)
+    -[Usage](#usage)
+    -[License](#license)
+    -[Contributing](#contributing)
+    -[Tests](#tests)
+    -[Questions](#questions)
+
+## Description:
+    ${data.description}
+## Installation:
+    ${data.installation}
+## Usage:
+    ${data.usage}
+## License:
+
+## Contributing
+    ${data.contributing}
+## Test
+    ${data.test}
+## Question
+    If you have any questions you can go to my Github page through this link:
+    -[Github](http://github.com/${data.github})
+
+    For additional questions please reach out to my email at: ${data.email}.
+`;
     return md;
 }
